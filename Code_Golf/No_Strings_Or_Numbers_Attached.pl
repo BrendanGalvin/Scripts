336d65b5e1530f7dd0782a6f5be6722c84e0856c
BrendanGalvin <codefun64@gmail.com>
2015-12-25 11:05:29 -0800

    Github broke my commitment streak. This saddens me.

---
+++ Code_Golf/No_Strings_Or_Numbers_Attached.pl
@@ -0,0 +1,16 @@
+# http://codegolf.stackexchange.com/questions/66921/no-strings-or-numbers-attached
+# It is unknown if this is a valid answer.
+# However theoretically, it might be allowed to post this 12 times as an individual code snippet.
+# Run with the -p command line argument.
+# Type a number, it will produce a number 1 greater than the number you typed...
+# Therefore, you can have it output any number, and there are no string literals or numeric literals in the code.
+
+
+$_++
+
+# Even better, you can just write $_ as the entire source code, and just type the number you want into the terminal.
+# Then, it will echo it back to you.
+
+# Or just have an empty program, run with the -p argument.
+
+# I feel like this is not in the spirit of the challenge, but nonetheless, would be the shortest solution!
