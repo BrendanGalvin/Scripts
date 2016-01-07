14fe9430f464d697115f1ba0da5f6a04cbfde106
BrendanGalvin <codefun64@gmail.com>
2016-01-06 21:21:03 -0800

    Taking a break from code golf to get some life stuff together. But I have some cool ideas for things to do with C#...

--- Code_Golf/Output_Double_Length_Of_Sourcecode.pl
+++ Code_Golf/Output_Double_Length_Of_Sourcecode.pl
@@ -1,4 +1,4 @@
 # Give it an integer argument and it will print a multiple of that integer.
 # Posted on http://codegolf.stackexchange.com/questions/59436/create-output-twice-the-length-of-the-code
 
-$_="n"x($_*18||36)
+$_ =$]x($_*2||4)
